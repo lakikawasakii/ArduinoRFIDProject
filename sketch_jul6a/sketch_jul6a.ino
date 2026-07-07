6260d1310ce63ed9849765beabf6b073dc75ded3
lakikawasakii <lazardenic08@gmail.com>
2026-07-07 14:33:07 +0200

    Servo Initialization

--- sketch_jul6a/sketch_jul6a.ino
+++ sketch_jul6a/sketch_jul6a.ino
@@ -1,9 +1,36 @@
-void setup() {
+#include <Servo.h>
+
+Servo servo;
+int signal;
+void setup()
+{
   Serial.begin(9600);
-  Serial.println("Initialized Succesfully");
+  servo.write(0);
+  servo.attach(8);
 }
 
-void loop() {
-  Serial.println("67");  
-  delay(1000);
+void loop()
+{ 
+  if(Serial.available() > 0)
+  {
+    signal  = Serial.parseInt();
+  }
+  if(signal == 1)
+  {
+    Serial.println("Received signal 1, doing 180 angle");
+    servo.write(180);
+    Serial.println("debug 0");
+  }
+  
+  else if(signal == 2)
+  {
+    Serial.println("Received signal 0, returning to 0");
+    servo.write(0);
+    Serial.println("debug 0");
+  }
+  else
+  {
+    Serial.println("Waiting for input value... ");
+  }
+  delay(500);
 }
